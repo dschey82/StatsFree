c7bd506624ea0b8ff43a3f8c8c37c3b764a3b866
Dan Schey <dschey@bluebeam.com>
2021-05-25 00:03:11 -0700

    Initial Commit

---
+++ StatsFree/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  StatsFree
+//
+//  Created by Dan Schey on 5/25/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ StatsFree/StatsFreeApp.swift
@@ -0,0 +1,17 @@
+//
+//  StatsFreeApp.swift
+//  StatsFree
+//
+//  Created by Dan Schey on 5/25/21.
+//
+
+import SwiftUI
+
+@main
+struct StatsFreeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
